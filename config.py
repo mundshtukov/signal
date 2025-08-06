077b9b97f2ed8333461fb8d85868617295dbc6ad
mundshtukov <47226485+mundshtukov@users.noreply.github.com>
2025-08-06 19:57:54 +0300

    Update config.py

--- config.py
+++ config.py
@@ -7,8 +7,8 @@ logger = logging.getLogger(__name__)
 # Получаем токен из переменных окружения
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 
-# API URLs
-BINANCE_API_URL = 'https://api.binance.com'
+# API URLs - переходим на Bybit
+BYBIT_API_URL = 'https://api.bybit.com'
 COINGECKO_API_URL = 'https://api.coingecko.com/api/v3'
 ALTERNATIVE_API_URL = 'https://api.alternative.me'
 
